4a3bbc6fda29301f59ca17f5bf84eeed875ef445
Dwayne Munro <dwaynemunro1845@gmail.com>
2018-09-08 18:43:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function moveDodgerRight() {
   var rightNumbers = dodger.style.right.replace("px", "");
   var right = parseInt(rightNumbers, 10);
   if (right > 0) {
-    
+    dodger.style.right = 
   }
 }
