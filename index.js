57cbf66921989d62135cc28aa0740d264d213bb4
Dwayne Munro <dwaynemunro1845@gmail.com>
2018-09-08 18:44:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const app = "I don't do much."
 var dodger = document.getElementById('dodger');
 
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerRight();
+  }
+})
+
 function moveDodgerRight() {
   var rightNumbers = dodger.style.right.replace("px", "");
   var right = parseInt(rightNumbers, 10);
